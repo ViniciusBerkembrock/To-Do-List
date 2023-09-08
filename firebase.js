8720663c8a485e1c6babd333f131b60939f7b418
ViniciusBerkembrock <113476905+ViniciusBerkembrock@users.noreply.github.com>
2023-09-08 16:36:24 -0300

    build: configuração firebase

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCvZZFQsEwWPFIc4o5n5DnC79_eTFDmbE4",
+  authDomain: "to-do-list-2b043.firebaseapp.com",
+  projectId: "to-do-list-2b043",
+  storageBucket: "to-do-list-2b043.appspot.com",
+  messagingSenderId: "382355352532",
+  appId: "1:382355352532:web:83f2f415a8cbe99319c441"
+};
+
+const app = initializeApp(firebaseConfig);
