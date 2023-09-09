799c70652da7f30fe1cf66a0b19bdaa9db41bf35
ViniciusBerkembrock <113476905+ViniciusBerkembrock@users.noreply.github.com>
2023-09-09 13:36:16 -0300

    test: adiciona teste de chamada das funções de authentication

--- src/pages/LoginPage/LoginPage.spec.jsx
+++ src/pages/LoginPage/LoginPage.spec.jsx
@@ -1,12 +1,42 @@
 import { render, screen, fireEvent, getByText } from "@testing-library/react"
 import { LoginPage } from "./LoginPage"
+import { signInWithPopup, signOut, getAuth } from "firebase/auth"
 
+jest.mock("firebase/auth", () => {
+    return {
+      getAuth: jest.fn(),
+      GoogleAuthProvider: jest.fn(),
+      signInWithPopup: jest.fn(),
+      signOut: jest.fn(),
+    };
+  });
 
 describe('LoginPage', () => {
 	it('should render correctly', () => {
 		render(<LoginPage/>);
 
         expect(screen.getByText('Sign In With Google')).toBeInTheDocument();
+        expect(screen.getByText('Sign Out')).toBeInTheDocument();
 	})
+
+    test("should call the signInWithPopup", () => {
+        render(<LoginPage />);
+ 
+        const signInButton = screen.getByText("Sign In With Google");
+        
+        fireEvent.click(signInButton);
+        
+        expect(signInWithPopup).toHaveBeenCalled();
+      });
+
+      test("should call the signOut", () => {
+        render(<LoginPage />);
+ 
+        const signInButton = screen.getByText("Sign Out");
+        
+        fireEvent.click(signInButton);
+        
+        expect(signOut).toHaveBeenCalled();
+      });
 
 })
