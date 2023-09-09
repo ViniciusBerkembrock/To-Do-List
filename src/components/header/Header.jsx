41e1e8b6cfebf5de63c1e6a5debaa8c4de558718
ViniciusBerkembrock <113476905+ViniciusBerkembrock@users.noreply.github.com>
2023-09-09 13:48:21 -0300

    feat: adiciona componente Header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import styles from './Header.module.css'
+
+export function Header() {
+    return(
+        <header className={styles.header}>
+            <h1>To Do List</h1>
+
+            <div>
+                <img src="" alt="" />
+                <span>Vinicius Berkembrock</span>
+            </div>
+
+            <button>LogOut</button>
+        </header>
+    )
+}
